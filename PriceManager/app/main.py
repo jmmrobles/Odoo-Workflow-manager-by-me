b70c0e0fb172dc47f76a5cb5c5cf929491655fc9
JMMR Gram <jmmrobles@sismg.com>
2026-03-29 01:44:27 -0500

    BaseApplication by JMMR

--- PriceManager/app/main.py
+++ PriceManager/app/main.py
@@ -7,7 +7,12 @@ from app.ui.app_window import AppWindow
 
 
 def run_app() -> None:
-    ctk.set_appearance_mode(settings.APPEARANCE_MODE)
-    ctk.set_default_color_theme(settings.COLOR_THEME)
-    app = AppWindow()
-    app.mainloop()
+    try:
+        ctk.set_appearance_mode(settings.APPEARANCE_MODE)
+        ctk.set_default_color_theme(settings.COLOR_THEME)
+
+        app = AppWindow()
+        app.mainloop()
+
+    except Exception as e:
+        print(f"Error al iniciar la aplicación: {e}")
